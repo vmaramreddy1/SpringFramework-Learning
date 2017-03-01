ad8f1255c16a37c51322fb09f82bf42b7bb2ea0d
Vijay Bhaskar Reddy Maramreddy <vijay.in.vnr@gmail.com>
2017-03-01 15:56:06 +0530

    restructured the folders

---
+++ SpringDemo1/src/main/java/org/springbyexample/di/xml/DefaultMessage.java
@@ -0,0 +1,20 @@
+package org.springbyexample.di.xml;
+
+public class DefaultMessage {
+	private String message = null;
+	
+//	public DefaultMessage(String message)
+//	{
+//		this.message = message;
+//	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
